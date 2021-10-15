cc49c5dce18bcba78ec9a0132d590d54e297c1db
baotianhua <baotianhua@csftgroup.com>
2021-10-15 18:18:08 +0800

    react-redux

---
+++ 1_src_redux完整版(包含异步action)/components/Count/index.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from 'react'
+import {
+    createIncrementAction,
+    createDecrementAction,
+    createIncrementAsyncAction
+} from '../../redux/count_action'
+import store from '../../redux/store'
+
+export default class Count extends Component {
+
+    state = {
+        carName: '奔驰c63'
+    }
+
+    // componentDidMount() {
+    //     store.subscribe(() => {
+    //         this.setState({})
+    //     })
+    // }
+
+    increment = () => {
+        const { value } = this.selectNumber
+        store.dispatch(createIncrementAction(value * 1))
+    }
+
+    decrement = () => {
+        const { value } = this.selectNumber
+        store.dispatch(createDecrementAction(value * 1))
+    }
+
+    incrementIfOdd = () => {
+        const { value } = this.selectNumber
+        const count = store.getState()
+
+        console.log('count', count);
+
+        if (count % 2 !== 1) {
+            return
+        }
+        store.dispatch(createIncrementAction(value * 1))
+    }
+
+    incrementAsync = () => {
+        const { value } = this.selectNumber
+        store.dispatch(createIncrementAsyncAction(value * 1, 500))
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>当前求和为：{store.getState()}</h1>
+                <select ref={c => this.selectNumber = c}>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                </select>&nbsp;
+                <button onClick={this.increment}>+</button>&nbsp;
+                <button onClick={this.decrement}>-</button>&nbsp;
+                <button onClick={this.incrementIfOdd}>当前求和为奇数再加</button>&nbsp;
+                <button onClick={this.incrementAsync}>异步加</button>&nbsp;
+            </div>
+        )
+    }
+}
